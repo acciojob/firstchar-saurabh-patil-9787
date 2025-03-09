1e3d6665019cb8df00752cd02c844a973b720850
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-09 10:32:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
   // your code here
+	if(text[0]=="" || text[0] ==" "){
+		return text[1]
+}else{
+return text[0]
+	}
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
